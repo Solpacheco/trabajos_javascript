89c22a6066844116b86e9470315dbd19081daed2
solpacheco@live.com.ar <solpacheco@live.com.ar>
2025-12-15 22:02:53 -0300

    Renombro CLASE9 a ejerciciosclase9

--- CLASE9/Ejercicio8.js
+++
@@ -1,3 +0,0 @@
-//Pide al usuario que ingrese las longitudes de los tres lados de un triángulo.
-//Determina y muestra si el triángulo es equilátero, isósceles o escaleno.
-// (Investiga sobre los triángulos para determinar la formula)
